2a3decf2dc6d06c088b06e5bed6c59c01f467780
Pham Tiep <phamkhactiep159@gmail.com>
2025-06-12 15:01:41 +0700

    update

--- src/main/java/CompatibilityTest.java
+++ src/main/java/CompatibilityTest.java
@@ -1,13 +1,10 @@
 import proto.New;
-import proto.Old;
-import proto.ProtoTest;
-import proto.Test;
-import proto.ProtoTest.*;
 
 public class CompatibilityTest {
     public static void run () throws Exception{
         Old old = Old.newBuilder().setAge(30).setName("Ha").build();
         byte[] sen = old.toByteArray();
         New  newObj = New.parseFrom(sen);
+        System.out.println(newObj);
     }
 }
